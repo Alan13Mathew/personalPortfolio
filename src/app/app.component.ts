b295fcdbf9c2843b683812d1bbe6a36383a5f1bf
Alan Mathew <alanmathew0891@gmail.com>
2024-08-09 00:09:42 +0530

    personal portfolio initial version without routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,CommonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio';
+  homeSelected = true;
+  projectSelected = false;
+  about = false;
+  aboutMe(){
+    this.about = true;
+    this.homeSelected = false;
+    this.projectSelected = false;
+  }
+  projects(){
+    this.projectSelected = true;
+    this.homeSelected = false;
+    this.about = false;
+  }
+  home(){
+    this.homeSelected = true;
+    this.projectSelected = false;
+    this.about = false;
+  }
 }
