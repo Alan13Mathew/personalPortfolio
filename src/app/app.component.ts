095f25a9fc9db6dc440bec65374a514aea5d5406
Alan Mathew <alanmathew0891@gmail.com>
2024-08-09 12:02:37 +0530

    used routes to specify routes instead of structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ,CommonModule ],
+  imports: [RouterOutlet ,CommonModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,6 +13,8 @@ export class AppComponent {
   homeSelected = true;
   projectSelected = false;
   about = false;
+
+  constructor(private router: Router){}
   aboutMe(){
     this.about = true;
     this.homeSelected = false;
@@ -24,8 +26,6 @@ export class AppComponent {
     this.about = false;
   }
   home(){
-    this.homeSelected = true;
-    this.projectSelected = false;
-    this.about = false;
+   
   }
 }
